c8f20290ef4b54ad8c202ed43e91df45fa95d0fe
Administrator <user@email.com>
2020-01-15 22:36:50 +0800

    开爬

--- run.py
+++ run.py
@@ -2,9 +2,19 @@ import requests
 import json
 
 
+# http://api.bilibili.com/archive_stat/stat?aid=8614334
 def getAvInfo(av):
     getAvInfoUrl = 'https://api.bilibili.com/x/web-interface/view?aid='
-    return json.loads(requests.get(getAvInfoUrl + av).text)
+    return json.loads(requests.get(getAvInfoUrl + str(av)).text)
+
 
 if __name__ == '__main__':
-    print(getAvInfo('22836761'))
+    maxView = 0
+    for av in range(1, 90000000):
+        print(av)
+        avInfo = getAvInfo(av)
+        if avInfo['code'] == 0:
+            view = avInfo['data']['stat']['view']
+            print('max = ' + str(maxView) + ', ' + avInfo['data']['title'] + ' ' + str(view))
+            if view > maxView:
+                maxView = view
